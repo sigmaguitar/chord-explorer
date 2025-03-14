0fe7994ab77f1d2f4384dfe3a50276b8ed346cea
Stefan <sjaklitsch@gmx.at>
2025-03-14 09:44:48 +0100

    obsolete json files + gui files deleted_modified

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -1,170 +1,105 @@
-import tkinter as tk
 import customtkinter as ctk
-from src.backend.scales import *
-from src.backend.triads import triad
-from src.backend.chords import chords
+import json
+import os
+import sys
 
-#TODO
+from backend.chords import chords
+from backend.triads import triad
 
-#module_path = os.path.abspath("C:/Users/s-jak/Documents/repositories/chord-explorer/src/backend")
-#sys.path.append(module_path)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(current_dir, '..','backend', 'data', "scales.json")
+
+with open(json_path, 'r', encoding='utf-8') as file:
+     data = json.load(file)
+
+module_path=os.path.abspath("/Users/s-jak/Documents/repositories/chord-explorer/src/backend")
+sys.path.append(module_path)
+
+from scales import *
+
+root = ctk.CTk()
+root.title("Chord Explorer")
+root.geometry('650x450')
+
+ctk.set_appearance_mode("dark") # system / light / dark
+ctk.set_default_color_theme("dark-blue") # dark-blue / green / blue
+
+tabview = ctk.CTkTabview(master=root)
+tabview.pack(padx=20, pady=20)
+tabview.add("Triads")
+tabview.add("Scales")
+tabview.add("Chords")
+
+
+def update_triads():
+    user_input = my_entry_triads.get()
+    output = triad(user_input, data)
+    output_triads.configure(text=output)
+
+def update_dropdown():
+    selected_value = combobox.get()
+    user_input = my_entry.get()
+    if selected_value in dropdown_map:
+        output = dropdown_map[selected_value](user_input, data)
+        output_scales.configure(text=output)
+
+def update_chords():
+    user_input = my_entry_chords.get()
+    output1, output2 = chords(user_input, data)
+    output_chords.configure(text=output1)
+    output_chords2.configure(text=output2)
 
 
 
-#from triads_function import *
-#from chords_function import chords
+title_scales = ctk.CTkLabel(master=tabview.tab("Scales"), text=" Explore Scales", font=("TimesNewRoman", 18))
+title_scales.pack(pady=0, padx=0)
 
-#
-#
-# root = ctk.CTk()
-# root.title("Chord Explorer")
-# root.geometry('650x450')
-#
-# ctk.set_appearance_mode("dark") # system / light / dark
-# ctk.set_default_color_theme("dark-blue") # dark-blue / green / blue
-#
-#
-# def update_label():
-#     user_input = my_entry.get()
-#     result = triads(user_input)
-#     label1.configure(text=result)
-#
-# def update_label2():
-#     user_input2 = my_entry2.get()
-#     result =chords(user_input2)
-#     label2.configure(text=result)
-#
-# def update_label3():
-#     user_input3 = my_entry3.get()
-#     result =pentatonic(user_input3)
-#     if user_input3 in maj:
-#         label4.configure(text=result)
-#     elif user_input3 in min:
-#         label3.configure(text=result)
-#
-# def update_dropdown():
-#     selected_value = combobox.get()
-#     user_input4 = my_entry4.get()
-#     if selected_value in dropdown_map:
-#         output = dropdown_map[selected_value](user_input4)
-#         label5.configure(text=output)
-#
-#
-# #frame = ctk.CTkFrame(master= root , fg_color="#8D6F3A", border_color="#FFCC70", border_width=2)
-# #frame.pack(expand=True)
-#
-# tabview = ctk.CTkTabview(master=root)
-# tabview.pack(padx=20, pady=20)
-#
-# tabview.add("Triads")
-# tabview.add("Chords")
-# tabview.add("Scales")
-# tabview.add("Dropdown")
-#
-#
-# # heading
-#
-# my_label = ctk.CTkLabel(master=tabview.tab("Triads"), text="TRIADS", font=("Arial", 20))
-# my_label.pack(pady=20, padx=20)
-#
-# my_label2 = ctk.CTkLabel(master=tabview.tab("Chords"), text="CHORDS", font=("Arial", 20))
-# my_label2.pack(pady=20, padx=20)
-#
-# my_label3 = ctk.CTkLabel(master=tabview.tab("Scales"), text=" PENTATONIC SCALE", font=("TimesNewRoman", 20))
-# my_label3.pack(pady=20, padx=20)
-#
-# my_label4 = ctk.CTkLabel(master=tabview.tab("Dropdown"), text=" Major / Natural minor / Pentatonic ", font=("TimesNewRoman", 20))
-# my_label4.pack(pady=20, padx=20)
-#
-#
-# # textboxes
-#
-# my_entry = ctk.CTkEntry(master=tabview.tab("Triads"),placeholder_text="   Key of Choice  ", font=("TimesNewRoman", 16))
-# my_entry.pack(pady=20,padx=10)
-#
-# my_entry2 = ctk.CTkEntry(master=tabview.tab("Chords"),placeholder_text=" Your Chosen Key  ", font=("TimesNewRoman", 16))
-# my_entry2.pack(pady=20,padx=10)
-#
-# my_entry3 = ctk.CTkEntry(master=tabview.tab("Scales"),placeholder_text=" Your Chosen Key  ", font=("TimesNewRoman", 16))
-# my_entry3.pack(pady=20,padx=10)
-#
-# my_entry4 = ctk.CTkEntry(master=tabview.tab("Dropdown"),placeholder_text="   Chosen Key  ", font=("TimesNewRoman", 16))
-# my_entry4.pack(pady=0,padx=10)
-#
-#
-# # buttons
-#
-# my_button = ctk.CTkButton(master=tabview.tab("Triads"), text="Explore",command=lambda:[update_label(), label_reset()], font=("TimesNewRoman", 16))
-# my_button.pack(pady=10)
-#
-# my_button2 = ctk.CTkButton(master=tabview.tab("Chords"), text="Explore",command=update_label2, font=("TimesNewRoman", 16))
-# my_button2.pack(pady=10)
-#
-# my_button3 = ctk.CTkButton(master=tabview.tab("Scales"), text="Explore",command=lambda:[update_label3(), label_reset()], font=("TimesNewRoman", 16))
-# my_button3.pack(pady=10)
-#
-# my_button4 = ctk.CTkButton(master=tabview.tab("Dropdown"), text="Explore", command=update_dropdown, font=("TimesNewRoman", 16))
-# my_button4.pack(pady=70)
-#
-#
-# dropdown_map = {"Major / Minor Scale": major_minor_scale , "Pentatonic Scale": pentatonic}
-#
-# combobox = ctk.CTkComboBox(master=tabview.tab("Dropdown"), values=["Major / Minor Scale", "Pentatonic Scale"], font=("TimesNewRoman", 14,))
-# combobox.place(relx=0.5, rely=0.5, anchor="center")
-#
-# combobox.bind("<<ComboboxSelected>>", update_dropdown)
-#
-#
-# #reset_button = ctk.CTkButton(master=tabview.tab("Scales"), text="RESET", font=("TimesNewRoman", 16))
-# #reset_button.pack(pady=5)
-#
-# maj_pentatonic_txt =" Major Pentatonic // 1-2-3-5-6"
-# min_pentatonic_txt="Minor Pentatonic // 1-b3-4-5-b7"
-# triads_txt ="Root Position - 1st Inversion - 2nd Inversion"
-#
-# click_count = 0
-#
-# def label_reset():
-#     global click_count
-#     click_count += 1
-#     if click_count == 2:
-#         label1.configure(text=triads_txt)
-#         label3.configure(text=maj_pentatonic_txt)
-#         label4.configure(text=min_pentatonic_txt)
-#         click_count = 0
-#
-# enter_count = 0
-#
-# def clear_entry(event=None):
-#     global enter_count
-#     enter_count += 1
-#     if enter_count == 1:
-#         my_entry.delete(0, tk.END)
-#         my_entry2.delete(0, tk.END)
-#         my_entry3.delete(0, tk.END)
-#         enter_count = 0
-#
-#
-# root.bind("<Return>", lambda event:[update_label(),update_label2(),update_label3(),clear_entry])
-# #root.bind("<Return>", clear_entry)
-#
-# label1 = ctk.CTkLabel(master=tabview.tab("Triads"), text= triads_txt, font=("Arial", 17))
-# label1.pack(pady=15)
-#
-# label2 = ctk.CTkLabel(master=tabview.tab("Chords"), text="Diatonic Chords", width=180, height=25,
-#                       corner_radius=8 ,font=("Arial", 17))
-# label2.pack(pady=15)
-#
-# label3 = ctk.CTkLabel(master=tabview.tab("Scales"), text= maj_pentatonic_txt, font=("Arial", 17))
-# label3.pack(pady=10)
-#
-# label4 = ctk.CTkLabel(master=tabview.tab("Scales"), text= min_pentatonic_txt, font=("Arial", 17))
-# label4.pack(pady=9)
-#
-# label5 = ctk.CTkLabel(master=tabview.tab("Dropdown"), text=" DROPWDOWN", font=("Arial", 17))
-# label5.pack(side="top", pady=0)
-#
-#
-#
-#
-# root.mainloop()
+title_triads = ctk.CTkLabel(master=tabview.tab("Triads"), text=" Explore Triads", font=("TimesNewRoman", 18))
+title_triads.pack(pady=0, padx=0)
+
+title_chords = ctk.CTkLabel(master=tabview.tab("Chords"), text=" Explore Chords ", font=("TimesNewRoman", 18))
+title_chords.pack(pady=0, padx=0)
+
+
+my_entry = ctk.CTkEntry(master=tabview.tab("Scales"),placeholder_text="   Choose a KEY  ", font=("TimesNewRoman", 16))
+my_entry.pack(pady=10,padx=10)
+
+my_entry_triads = ctk.CTkEntry(master=tabview.tab("Triads"),placeholder_text="   Choose a KEY  ", font=("TimesNewRoman", 16))
+my_entry_triads.pack(pady=10,padx=10)
+
+my_entry_chords = ctk.CTkEntry(master=tabview.tab("Chords"),placeholder_text="   Choose a KEY  ", font=("TimesNewRoman", 16))
+my_entry_chords.pack(pady=10,padx=10)
+
+combobox = ctk.CTkComboBox(master=tabview.tab("Scales"), values=[" Major | Minor", " Pentatonic", " Melodic Minor",
+                                                                 " Harmonic Minor", " Blues Scale"], font=("TimesNewRoman", 16,))
+combobox.pack(pady=0, padx=0, anchor="center")
+
+dropdown_map = {" Major | Minor": major_minor_scale , " Pentatonic": pentatonic, " Melodic Minor": melodic_minor_scale,
+                " Harmonic Minor": harmonic_minor_scale, " Blues Scale": blues_scale}
+
+
+my_button = ctk.CTkButton(master=tabview.tab("Scales"), text="Explore",command=update_dropdown,font=("TimesNewRoman", 16))
+my_button.pack(pady=20)
+
+my_button_triads = ctk.CTkButton(master=tabview.tab("Triads"), text="Explore",command=update_triads,font=("TimesNewRoman", 16))
+my_button_triads.pack(pady=5)
+
+my_button_chords = ctk.CTkButton(master=tabview.tab("Chords"), text="Explore",command=update_chords,font=("TimesNewRoman", 16))
+my_button_chords.pack(pady=5)
+
+
+output_scales = ctk.CTkLabel(master=tabview.tab("Scales"), text="  ", font=("Arial", 17))
+output_scales.pack(side="top", pady=0)
+
+output_triads = ctk.CTkLabel(master=tabview.tab("Triads"), text=" TRIADS ", font=("Arial", 17))
+output_triads.pack(side="top", padx=10, pady=10)
+
+output_chords = ctk.CTkLabel(master=tabview.tab("Chords"), text="  ", font=("Arial", 17))
+output_chords.pack(side="top", padx=10, pady=10)
+
+output_chords2 = ctk.CTkLabel(master=tabview.tab("Chords"), text="  ", font=("Arial", 17))
+output_chords2.pack(side="top", padx=10, pady=10)
+
+root.mainloop()
+
+print(sys.path)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,3 @@ from frontend.gui import *
 
 if __name__ == '__main__':
     pass    # ToDo
-
-
-# with open('data/scales.json', 'r') as file:
-# data = json.load(file)
